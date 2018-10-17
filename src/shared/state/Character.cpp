fc8abce034c2a73ec62fc35dace9aadb8c1c4b79
Fyrex <louisquelliermsn@hotmail.fr>
2018-10-17 15:30:33 +0200

    setter for position character

--- src/shared/state/Character.cpp
+++ src/shared/state/Character.cpp
@@ -18,12 +18,21 @@ Character::Character(std::size_t i,
   this->pmMax = pm;
 }
 
+// Getter
 std::size_t Character::getI() {
   return i;
 }
 
 std::size_t Character::getJ() {
   return j;
+}
+
+// Setter
+void Character::setI(std::size_t i) {
+  this->i = i;
+}
+void Character::setJ(std::size_t j) {
+  this->j = j;
 }
 
 }  // namespace state
