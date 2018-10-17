74cae46575511ec2785e0a31fb70452a873f7a70
Fyrex <louisquelliermsn@hotmail.fr>
2018-10-17 11:42:07 +0200

    Weapon, update some del function

--- src/shared/state/Weapon.cpp
+++ src/shared/state/Weapon.cpp
@@ -1,3 +1,26 @@
 #include "Weapon.h"
 
 using namespace state;
+
+Weapon::Weapon(ElementType element, std::vector<Ability*> abilities) {
+  this->element = element;
+  this->abilities = abilities;
+}
+
+void Weapon::addAbility(Ability& ability) {
+  abilities.push_back(&ability);
+}
+
+std::vector<Ability*> Weapon::getAbilities() {
+  return abilities;
+}
+
+void Weapon::delAbility(Ability* pAbility) {
+  for (auto it = abilities.begin(); it != abilities.end(); it++) {
+    Ability* pa = *it;
+    if (pa == pAbility) {
+      abilities.erase(it);
+      return;
+    }
+  }
+}
