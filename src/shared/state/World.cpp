e68c5fe5a491afd3ae81619a22144174eae62128
Fyrex <louisquelliermsn@hotmail.fr>
2018-10-18 16:35:31 +0200

    Fix seg fault dans del char

--- src/shared/state/World.cpp
+++ src/shared/state/World.cpp
@@ -4,48 +4,53 @@ using namespace std;
 using namespace state;
 
 World::World(size_t i, size_t j) {
-    // Initialize the grid with the good dimension and fill them with empty cell
-    I = i;
-    J = j;
-    grid.resize(i);
-    for (size_t k = 0; k < i; k++) {
-        grid[k].resize(j);
-        for (size_t l = 0; l < j; l++) {
-            Cell* cell = new Cell();
-            grid[k][l] = cell;
-        }
+  // Initialize the grid with the good dimension and fill them with empty cell
+  I = i;
+  J = j;
+  grid.resize(i);
+  for (size_t k = 0; k < i; k++) {
+    grid[k].resize(j);
+    for (size_t l = 0; l < j; l++) {
+      Cell* cell = new Cell();
+      grid[k][l] = cell;
     }
+  }
 }
 
 vector<vector<Cell*>> World::getGrid() {
-    return grid;
+  return grid;
 }
 
 Cell* World::getCell(size_t i, size_t j) {
-    return grid[i][j];
+  return grid[i][j];
 }
 
 void World::setCell(size_t i, size_t j, Cell* cell) {
-    grid[i][j] = cell;
+  grid[i][j] = cell;
 }
 
 void World::addCharacter(Character* character) {
-    characters.push_back(character);
+  characters.push_back(character);
 }
 
 void World::delCharacter(size_t i, size_t j) {
-    for (auto c = characters.begin(); c != characters.end(); ++c) {
-        if ((*c)->getI() == i && (*c)->getJ() == j) characters.erase(c);
+  for (auto it = characters.begin(); it != characters.end(); ++it) {
+    Character* pc = *it;
+    if (pc->getI() == i && pc->getJ() == j) {
+      characters.erase(it);
+      return;
     }
+  }
 }
 
 Character* World::getCharacter(size_t i, size_t j) {
-    for (auto c = characters.begin(); c != characters.end(); ++c) {
-        if ((*c)->getI() == i && (*c)->getJ() == j) return *c;
-    }
-    return nullptr;
+  for (auto c = characters.begin(); c != characters.end(); ++c) {
+    if ((*c)->getI() == i && (*c)->getJ() == j)
+      return *c;
+  }
+  return nullptr;
 }
 
 vector<Character*> World::getCharacters() {
-    return characters;
+  return characters;
 }
